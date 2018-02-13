60d341dbac3a699dbbd1a50c410aa704436f34af
muca <mucisan22@gmail.com>
2018-02-13 22:17:06 +0100

    Initial commit

---
+++ src/arraylist/ArrayLists.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arraylist;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author mucis
+ */
+public class ArrayLists {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("test");
+        
+        
+    }
+    
+}
