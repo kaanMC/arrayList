ada9571bcd934ef5ab5f6e8b9566d4f76c732312
mucsian <36458212+mucsian@users.noreply.github.com>
2018-02-17 23:54:06 +0100

    Update ArrayLists.java

--- src/arraylist/ArrayLists.java
+++ src/arraylist/ArrayLists.java
@@ -21,7 +21,6 @@ public class ArrayLists {
         names.add("test");
         names.add("test2");
         names.add("Git");
-        Oh shit;
         System.out.println(names.get(2));
         
     } 
